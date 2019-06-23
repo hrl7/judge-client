2f7beeba40cbf2428047194e0f0f016b18e162b7
hrl7 <zigen@horol.org>
2019-06-23 18:59:37 +0900

    add scheme-interpreter

--- src/pages/Problem.js
+++ src/pages/Problem.js
@@ -3,7 +3,29 @@ import {connect} from "react-redux";
 import Navbar from "../components/navbar";
 import Loading from "../components/Loading";
 
+import {REPL, Lexer, Parser} from "scheme-interpreter";
+
 class Problem extends Component {
+  constructor(props) {
+    super(props);
+    this.repl = new REPL();
+    this.state = {
+      src: "",
+      results: []
+    };
+    this.handleEdit = this.handleEdit.bind(this);
+    this.run= this.run.bind(this);
+  }
+
+  handleEdit(e) {
+    this.setState({src: e.target.value});
+  }
+
+  run(e) {
+    this.repl.run(this.state.src);
+    this.setState({results: [...this.state.results, this.repl.print()]});
+  }
+
   render() {
     const {problem} = this.props;
     if (!problem) {
@@ -14,17 +36,40 @@ class Problem extends Component {
     return (<div>
       <Navbar/>
       <h1>Problem: {name}</h1>
-      <div>
-        <div>{statement}</div>
-
+      <div>{statement} by <span>{author.email}</span></div>
+      <div className="playground-container">
+        <div className="editor-container">
+          <h2>Editor</h2>
+          <button onClick={this.run}>run</button>
+          <textarea onChange={this.handleEdit}/>
+        </div>
+        <div className="repl-container">
+          <h2>Result</h2>
+          <pre>
+          {this.state.results.join("\n")}
+          </pre>
+        </div>
       </div>
+      <style jsx>{`
+        .playground-container {
+           display: flex;
+           justify-content: space-around;
+        } 
+        
+        .playground-container > div {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+        }
+        
+      `}</style>
     </div>)
   }
 }
 
 const mapStateToProps = (state) => ({
-  problem: state.problems.list.find(p => p.id === state.problems.current)
-})
+    problem: state.problems.list.find(p => p.id === state.problems.current)
+  })
 ;
 const mapDispatchToProps = (dispatch) => ({});
 
